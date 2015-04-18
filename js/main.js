51aa46b9c189942b3e8b328d26d74659047f196b
kbeazer <kbeazer@fullsail.edu>
2015-04-18 10:05:48 -0400

    projects.html updated

--- js/main.js
+++ js/main.js
@@ -2,9 +2,64 @@ $(document).ready(function()
 {
 	console.log("I'm working!!!")
 	
-	$('.displayBox a').click(function(e)
+	$('.box1').click(function(e)
 	{
 		e.preventDefault();
 		$('.phide').slideToggle('slow');
 	});
+
+	$('.box2').click(function(e)
+	{
+		e.preventDefault();
+		$('.phide2').slideToggle('slow');
+	});
+
+	$('.box3').click(function(e)
+	{
+		e.preventDefault();
+		$('.phide3').slideToggle('slow');
+	});
+
+	$('.box4').click(function(e)
+	{
+		e.preventDefault();
+		$('.phide4').slideToggle('slow');
+	});
+
+	$('.box5').click(function(e)
+	{
+		e.preventDefault();
+		$('.phide5').slideToggle('slow');
+	});
+
+	$('.box6').click(function(e)
+	{
+		e.preventDefault();
+		$('.phide6').slideToggle('slow');
+	});
+
+	$('.box7').click(function(e)
+	{
+		e.preventDefault();
+		$('.phide7').slideToggle('slow');
+	});
+
+	$('.box8').click(function(e)
+	{
+		e.preventDefault();
+		$('.phide8').slideToggle('slow');
+	});
+
+	$('.box9').click(function(e)
+	{
+		e.preventDefault();
+		$('.phide9').slideToggle('slow');
+	});
+
+	$('.box10').click(function(e)
+	{
+		e.preventDefault();
+		$('.phide10').slideToggle('slow');
+	});
+
 });
