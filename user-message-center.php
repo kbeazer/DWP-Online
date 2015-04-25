695b363b617da452b423c504996e8df8e1eb9fe7
kbeazer <kbeazer@fullsail.edu>
2015-04-24 20:42:09 -0400

    message center added to staging server

--- user-message-center.php
+++ user-message-center.php
@@ -1,22 +1,49 @@
-<?php
-    // Kareem K. Beazer
-	session_start();
+<!DOCTYPE html>
+<html class="no-js" lang="en">
 
-    $user = "kareem";
-    $pass = "kbeazer13";
-    $sql = "mysql:host=104.236.52.251; dbname=kbcreations; port=3306;";
-    $dbh = new PDO($sql, $user, $pass);
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Kbeazer Creations</title>
+        <link href="css/style.css" rel="stylesheet" />
+        <link href='http://fonts.googleapis.com/css?family=Michroma' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'>
+    </head>
 
-    $query = "SELECT * FROM messages;";
-    $stmt = $dbh -> prepare($query);
-    $stmt -> execute();
-    $results = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+    <body> 
+        <div class="container">
+            <div>
+                <h2>KB Creations Message Center</h2>
+            </div>
+            <?php
+                $user = "kareem";
+                $pass = "kbeazer13";
+                $sql = "mysql:host=104.236.52.251; dbname=kbcreations; port=3306;";
+                $dbh = new PDO($sql, $user, $pass);
 
-    foreach ($results as $message) {
-       echo '<div class="userRecord">';
-       echo "<h3>" . $message["userName"] . "</h3>";
-       echo "<p>Email: " . $message["email"] . "</p>";
-       echo "<p>Message: " . $message["message"] . "</p>";
-       echo "</div>";
-   };
-?>
+                $query = "SELECT * FROM messages;";
+                $stmt = $dbh -> prepare($query);
+                $stmt -> execute();
+                $results = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+
+                foreach ($results as $message) {
+                   echo '<div class="userRecord">';
+                   echo "<h3>" . $message["userName"] . "</h3>";
+                   echo "<p>Email: " . $message["email"] . "</p>";
+                   echo "<p>Message: " . $message["message"] . "</p>";
+                   echo "</div>";
+               };
+            ?>
+
+            <footer>
+                <p>Designed and Developed by Kareem K. Beazer. All rights reserved 2015 &copy;</p>
+            </footer>
+        </div> 
+        <script type="text/javascript" src="js/jquery-1.11.0.js"></script>
+        <script type="text/javascript" src="js/main.js"></script>
+    </body>
+</html>
+
+
+
+
